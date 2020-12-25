74742ac1aeefae36c659b81f17bf452197c71b02
Anthony Kieu <anthonykieu@gmail.com>
2020-12-25 17:16:46 -0500

    added partial

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
-
-const hbs = exphbs.create({});
-const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
@@ -19,6 +18,13 @@ const sess = {
     db: sequelize
   })
 };
+
+app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -27,8 +33,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
-app.use(session(sess));
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
 
